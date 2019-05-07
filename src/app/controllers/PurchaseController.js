43078b59b25bdc74accc86fafb03f090be6de818
Lucas Moreira <eskerlucas1@gmail.com>
2019-05-07 20:02:48 -0300

    Send mail in background with Redis Queue

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,21 +1,18 @@
 const Ad = require('../models/ad')
 const User = require('../models/User')
-const Mail = require('../services/Mail')
+const PurchaseMail = require('../jobs/PurchaseMail')
+const Queue = require('../services/Queue')
 
 class PurchaseController {
   async store (req, res) {
     const { ad, content } = req.body
     const purchaseAd = await Ad.findById(ad).populate('author')
     const user = await User.findById(req.userId)
-
-    await Mail.sendMail({
-      from: '"Lucas Moreira" <eskerlucas1@gmail.com>',
-      to: purchaseAd.author.email,
-      subject: `Solicitação de compra: ${purchaseAd.title}`,
-      template: 'purchase',
-      context: { user, content, ad: purchaseAd }
-    })
-
+    Queue.create(PurchaseMail.key, {
+      ad: purchaseAd,
+      user,
+      content
+    }).save()
     return res.send()
   }
 }
