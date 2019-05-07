21b87bffeafcbcdf1aef88028fce58cbdb512ddb
Lucas Moreira <eskerlucas1@gmail.com>
2019-05-06 23:04:05 -0300

    Add auth middleware

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ const express = require('express')
 
 const routes = express.Router()
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+const controllers = require('./app/controllers')
+
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
